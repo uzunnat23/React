5f491716d8aa691da9759e65c6e0ece803ff9387
unknown <uzunnat23@gmail.com>
2021-07-18 16:46:18 +0300

    Connecting State to Build Controls

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/BuildControls/BuildControls.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/BuildControls/BuildControls.js
@@ -14,7 +14,12 @@ const controls = [
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
     {controls.map((ctrl) => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        type={ctrl.type}
+        added={() => props.ingredientAdded(ctrl.type)}
+      />
     ))}
   </div>
 );
