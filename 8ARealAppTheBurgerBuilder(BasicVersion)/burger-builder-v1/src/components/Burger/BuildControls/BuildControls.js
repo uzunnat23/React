755444e1511950a2be4b0be73c78eb2a69adaeb7
unknown <uzunnat23@gmail.com>
2021-08-10 10:09:01 +0300

    Displaying and Updating the Burger Price

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/BuildControls/BuildControls.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,9 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>
+      Curren Price:<strong>{props.price.toFixed(2)}</strong>{" "}
+    </p>
     {controls.map((ctrl) => (
       <BuildControl
         key={ctrl.label}
