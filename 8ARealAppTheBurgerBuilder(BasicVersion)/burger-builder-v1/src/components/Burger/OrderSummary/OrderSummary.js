a3fc400dc438ff930121fdcdedc8aa4740907cb9
unknown <uzunnat23@gmail.com>
2021-08-19 19:14:31 +0300

    Implementing the Button Component

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/OrderSummary/OrderSummary.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Aux from "../../../hoc/Auxiliary";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -11,14 +12,19 @@ const orderSummary = (props) => {
       </li>
     );
   });
+
   return (
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious buurger witn the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-      <button>Cancel</button>
-      <button>Comtinue</button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        Continue
+      </Button>
     </Aux>
   );
 };
--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/containers/BurgerBulder/BurgerBuilder.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/containers/BurgerBulder/BurgerBuilder.js
@@ -79,6 +79,9 @@ class BurgerBuilder extends Component {
   purchaseCancelHandler = () => {
     this.setState({ purchasing: false });
   };
+  purchaseContinueHandler = () => {
+    alert("You continue! ");
+  };
 
   render() {
     const disabledInfo = {
@@ -95,7 +98,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
