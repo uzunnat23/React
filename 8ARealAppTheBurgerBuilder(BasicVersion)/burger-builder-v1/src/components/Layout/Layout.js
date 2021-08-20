548d831c3b08f8744351d6c920e5da9bfd9f6132
unknown <uzunnat23@gmail.com>
2021-08-20 14:29:52 +0300

    Creating a Responsive Sidedrawer

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Layout/Layout.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 import classes from "./Layout.module.css";
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
