b5ec9275f2747ad79addaa8a63b8637629e7ef61
unknown <uzunnat23@gmail.com>
2021-08-20 14:06:25 +0300

    Adding Resable Navigation Items

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Navigation/Toolbar/Toolbar.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 import classes from "./Toolbar.module.css";
 
@@ -8,7 +9,9 @@ const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 export default toolbar;
