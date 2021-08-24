a460126acd7f46dd34d5a00eb073f5604b852b58
unknown <uzunnat23@gmail.com>
2021-08-24 16:17:49 +0300

    Improving Performance

--- 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/OrderSummary/OrderSummary.js
+++ 8ARealAppTheBurgerBuilder(BasicVersion)/burger-builder-v1/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  //This could be a functional cmpt, does't have to be a class
+  componentWillUpdate() {
+    console.log("[OrderSummary] WillUpdate");
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious buurger witn the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price} </strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        Continue
-      </Button>
-    </Aux>
-  );
-};
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious buurger witn the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price} </strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </Aux>
+    );
+  }
+}
+export default OrderSummary;
