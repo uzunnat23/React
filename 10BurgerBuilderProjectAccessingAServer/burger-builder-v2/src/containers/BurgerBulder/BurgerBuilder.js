43bbd8e2f48b428046be3667f0fc75f59dabb682
unknown <uzunnat23@gmail.com>
2021-08-26 17:20:19 +0300

    Sending a POST Request

--- 10BurgerBuilderProjectAccessingAServer/burger-builder-v2/src/containers/BurgerBulder/BurgerBuilder.js
+++ 10BurgerBuilderProjectAccessingAServer/burger-builder-v2/src/containers/BurgerBulder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -80,7 +81,25 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
   purchaseContinueHandler = () => {
-    alert("You continue! ");
+    //alert("You continue! ");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Nataly",
+        address: {
+          street: "Qwerty",
+          zipCode: "1234",
+          country: "Moldova",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
