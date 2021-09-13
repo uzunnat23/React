82b8cf332d540fa6e6dced3db32015a11dc403a4
unknown <uzunnat23@gmail.com>
2021-09-13 18:34:30 +0300

    Preperin the Project For Routing

--- 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
+++ 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
@@ -1,57 +1,11 @@
 import React, { Component } from "react";
-// import axios from 'axios';
-import axios from "../../axios";
 
-import Post from "../../components/Post/Post";
+import Posts from "./Posts/Posts";
 
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Nataly",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-        // console.log( response );
-      })
-      .catch((error) => {
-        // console.log(error);
-        this.setState({ error: true });
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -66,7 +20,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
