076d15e2f0b01022ca78059a85cac63d9aaf5c34
unknown <uzunnat23@gmail.com>
2021-09-13 17:13:02 +0300

    Removing Old Interceptors

--- 10BurgerBuilderProjectAccessingAServer/burger-builder-v2/src/hoc/withErrorHandler/withErrorHandler.js
+++ 10BurgerBuilderProjectAccessingAServer/burger-builder-v2/src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,11 +9,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
@@ -21,6 +21,10 @@ const withErrorHandler = (WrappedComponent, axios) => {
       );
     }
 
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
     errorConfirmedHandler = () => {
       this.setState({ error: null });
     };
