57869dc65249ffb5538186b80fe7127fb8847a9c
unknown <uzunnat23@gmail.com>
2021-09-14 16:19:55 +0300

    Switching Between Pages

--- 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
+++ 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 import "./Blog.css";
 
@@ -25,6 +26,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home2</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
