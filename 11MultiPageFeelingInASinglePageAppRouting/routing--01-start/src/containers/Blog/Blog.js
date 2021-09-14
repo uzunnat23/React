f764884cfc919326d87c61fa37d841c9077fb047
unknown <uzunnat23@gmail.com>
2021-09-14 16:28:27 +0300

    Using Links to Switch Pages

--- 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
+++ 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
@@ -15,10 +15,19 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
+
               <li>
-                <a href="/new-post">New Post</a>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
