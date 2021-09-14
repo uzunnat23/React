b288e9460d1b0fe9e17b0e3cd6990429bc557333
unknown <uzunnat23@gmail.com>
2021-09-14 16:06:06 +0300

    Setting Up and Rendering Routs

--- 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
+++ 11MultiPageFeelingInASinglePageAppRouting/routing--01-start/src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { Route } from "react-router-dom";
+
 import Posts from "./Posts/Posts";
 
 import "./Blog.css";
@@ -20,7 +22,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home2</h1>} />
       </div>
     );
   }
